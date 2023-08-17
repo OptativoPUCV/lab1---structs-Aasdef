421f05cb356d64e5704e88db085a1799495c7a9d
Replit user <>
2023-08-17 04:06:23 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -96,7 +96,7 @@ y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
 */
 int checkSorted(int arr[], int size) { 
   for (int i=0; i<size; i++){
-    if (arr[i]>arr[i-1]){
+    if (arr[i]>arr[i+1]){
       //noesascendente
       break;
     }
